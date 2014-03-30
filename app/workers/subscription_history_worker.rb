60c0b3e7e41722e54b29f079f5c345c6fad89f08
Fari Liang <fari@Fari-Liangs-iMac.local>
2014-03-30 06:23:22 -0400

    change redis config

--- app/workers/subscription_history_worker.rb
+++
@@ -1,8 +0,0 @@
-class SubscriptionHistoryWorker
-
-	#could be retrying all the time, this has to be thread save
-
-	def perform(subscription_id)
-
-	end	
-end
--- config/application.rb
+++ config/application.rb
@@ -84,6 +84,13 @@ module Ratafire
     #    config.cache_store = :redis_store, 'redis://localhost:6379/0/cache', { expires_in: 90.minutes }
     #end
     
+# Load the redis.yml configuration file 
+redis_config = YAML.load_file(Rails.root + 'config/redis.yml')[Rails.env] 
+
+# Connect to Redis using the redis_config host and port 
+if redis_config 
+  $redis = Redis.new(host: redis_config['host'], port: redis_config['port']) 
+end 
 
   end
 end
