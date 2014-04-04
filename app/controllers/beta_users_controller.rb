f519bab82d407797077b32f38566d42f042ca0dd
Fari Liang <fari@Fari-Liangs-iMac.local>
2014-04-04 17:56:22 -0400

    Change amazon payments

--- app/controllers/beta_users_controller.rb
+++ app/controllers/beta_users_controller.rb
@@ -18,7 +18,23 @@ class BetaUsersController < ApplicationController
 	end
 
 	def show
-		
+		@beta_users = BetaUser.where(:approved => nil, :ignore => nil).paginate(page: params[:page], :per_page => 50)
+		@beta_art = BetaUser.where(:approved => nil, :ignore => nil, :realm => 'art', :creator => true).paginate(page: params[:page], :per_page => 50)
+		@beta_music = BetaUser.where(:approved => nil, :ignore => nil, :realm => 'music', :creator => true).paginate(page: params[:page], :per_page => 50)
+		@beta_games = BetaUser.where(:approved => nil, :ignore => nil, :realm => 'games', :creator => true).paginate(page: params[:page], :per_page => 50)
+		@beta_writing = BetaUser.where(:approved => nil, :ignore => nil, :realm => 'writing', :creator => true).paginate(page: params[:page], :per_page => 50)
+		@beta_videos = BetaUser.where(:approved => nil, :ignore => nil, :realm => 'videos', :creator => true).paginate(page: params[:page], :per_page => 50)
+		@beta_math = BetaUser.where(:approved => nil, :ignore => nil, :realm => 'math', :creator => true).paginate(page: params[:page], :per_page => 50)
+		@beta_science = BetaUser.where(:approved => nil, :ignore => nil, :realm => 'science', :creator => true).paginate(page: params[:page], :per_page => 50)
+		@beta_humanity = BetaUser.where(:approved => nil, :ignore => nil, :realm => 'humanity', :creator => true).paginate(page: params[:page], :per_page => 50)
+		@beta_engineering = BetaUser.where(:approved => nil, :ignore => nil, :realm => 'engineering', :creator => true).paginate(page: params[:page], :per_page => 50)
+		@beta_non = BetaUser.where(:approved => nil, :ignore => nil, :creator => nil).paginate(page: params[:page], :per_page => 50)
+	end
+
+	def approve
+	end
+
+	def ignore
 	end
 
 private
