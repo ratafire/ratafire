29ca0f262e1db73de784c47d93cb508cc43c9271
Fari Liang <fari@Fari-Liangs-iMac.local>
2014-04-05 00:39:18 -0400

    Correct Invitation Email Broken Links

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Ratafire::Application.configure do
   config.assets.precompile += %w( video-js.swf vjs.eot vjs.svg vjs.ttf vjs.woff )
 
   #Devise Mailing
-  config.action_mailer.default_url_options = { :host => '#{request.scheme}://#{request.host}#{port}' }
+  config.action_mailer.default_url_options = { :host => 'https://www.ratafire.com' }
   config.action_mailer.asset_host = '#{request.scheme}://#{request.host}#{port}'
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
