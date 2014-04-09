1d6a6b106edf6c0615027738154348b5e5160235
Fari Liang <fari@Fari-Liangs-iMac.local>
2014-04-08 22:49:42 -0400

    Thinking-sphinx config

--- deploy/before_migrate.rb
+++
@@ -1,2 +0,0 @@
-run "ln -nfs #{config.shared_path}/config/sphinx #{config.release_path}/config/sphinx" 
-run "ln -nfs #{config.shared_path}/config/sphinx.yml #{config.release_path}/config/sphinx.yml" 
