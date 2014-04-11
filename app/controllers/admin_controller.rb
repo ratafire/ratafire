f7098b555cb8043590a1bc08c29cc4c57fe7b203
Fari Liang <fari@Fari-Liangs-iMac.local>
2014-04-11 13:36:08 -0400

    Test private work

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,6 @@
+class AdminController < ApplicationController
+	layout 'application'
+
+	def test
+	end
+end
--- app/workers/test_worker.rb
+++ app/workers/test_worker.rb
@@ -2,5 +2,13 @@ class TestWorker
 	@queue = :test_queue
 
 	def self.perform
+		test = 1+1
+		self.privatework
 	end
+
+private
+
+	def self.privatework
+		test = 2+1
+	end	
 end
