35c5567492d062420c3babb8f5afacab07fe4a2c
Fari Liang <fari@Fari-Liangs-iMac.local>
2014-06-25 02:50:27 -0400

    Fixing Billing Bug

--- app/workers/subscription_worker.rb
+++ app/workers/subscription_worker.rb
@@ -1,11 +1,11 @@
 class SubscriptionWorker
-	@queue = :subscriptions_queue
-	def self.perform()
-		subscriptions = Subscription.where(:activated => true, :deleted_at => nil)
-		if subscriptions.count != 0 then
-			subscriptions.each do |s|
-				Resque.enqueue(TransactionWorker, s.id)
-			end
-		end
-	end
+#	@queue = :subscriptions_queue
+#	def self.perform()
+#		subscriptions = Subscription.where(:activated => true, :deleted_at => nil)
+#		if subscriptions.count != 0 then
+#			subscriptions.each do |s|
+#				Resque.enqueue(TransactionWorker, s.id)
+#			end
+#		end
+#	end
 end
