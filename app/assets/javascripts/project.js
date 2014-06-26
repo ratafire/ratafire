d7c242af07cab3c41b7423360600a24dd76ce90a
Fari Liang <fari@Fari-Liangs-iMac.local>
2014-06-25 20:00:49 -0400

    Change Post Images

--- app/assets/javascripts/project.js
+++ app/assets/javascripts/project.js
@@ -1,11 +1,12 @@
 // assets/javascripts/documents.js
-$(function() {
 
+$(function() {
  	//project video uploader	
   $('#s3_uploader_video').S3Uploader(
     { 
       remove_completed_progress_bar: false,
-      progress_bar_target: $('#video-progress-case')
+      progress_bar_target: $('#video-progress-case'),
+      dropZone: $('#nodropzone')
     }
   ,"video"); //ratafire_file_type,content_temp_value,content_temp_video_value,tags_temp_video_value
   $('#s3_uploader_video').bind('s3_upload_failed', function(e, content) {
@@ -16,7 +17,8 @@ $(function() {
   $('#s3_uploader_artwork').S3Uploader(
     { 
       remove_completed_progress_bar: false,
-      progress_bar_target: $('#artwork-progress-case')
+      progress_bar_target: $('#artwork-progress-case'),
+      dropZone: $('#nodropzone')
     }
   ,"artwork"); //ratafire_file_type,content_temp_value,content_temp_video_value,tags_temp_video_value
   $('#s3_uploader_artwork').bind('s3_upload_failed', function(e, content) {
@@ -27,12 +29,14 @@ $(function() {
   $('#s3_uploader_icon').S3Uploader(
     { 
       remove_completed_progress_bar: false,
-      progress_bar_target: $('#icon-progress-case')
+      progress_bar_target: $('#icon-progress-case'),
+      dropZone: $('#nodropzone')
     }
   ,"icon"); //ratafire_file_type,content_temp_value,content_temp_video_value,tags_temp_video_value
   $('#s3_uploader_icon').bind('s3_upload_failed', function(e, content) {
     return alert(content.filename + ' failed to upload');
   });  
+
 
 
 });
