ab819f55ff4e4b8efd0af19571414d9c2d73e00d
fariliang <fariliang@gmail.com>
2014-12-30 19:43:24 -0500

    get rid of water

--- db/migrate/20141229222407_add_profile_direct_upload_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddProfileDirectUploadToUsers < ActiveRecord::Migration
-  def change
-  	add_column :users, :direct_upload_url, :string
-  	add_column :users, :processed, :boolean, :default => false
-  	add_column :users, :skip_everafter, :default => false
-  end
-end
