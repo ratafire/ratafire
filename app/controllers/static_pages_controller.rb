9c7565b5374e4d6b82aeda296cee3cf5a89e9995
fariliang <fariliang@gmail.com>
2015-01-21 20:52:24 -0500

    homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,7 +8,7 @@ class StaticPagesController < ApplicationController
   def home
     @activities = PublicActivity::Activity.order("commented_at desc").where(:featured_home => true).paginate(page: params[:page], :per_page => 3)
   	@user = current_user
-    @featured_user = User.find(3)
+    @featured_user = User.find_by_username("colinchromatic")
   	unless signed_in?
     else
       redirect_to @user
