e00c5d5e327c77481f14c20fb1e30de99dc30968
fariliang <fariliang@gmail.com>
2015-02-27 22:07:55 -0500

    Migration

--- db/migrate/20150218013039_add_countdown_to_subscription.rb
+++ db/migrate/20150218013039_add_countdown_to_subscription.rb
@@ -1,6 +1,6 @@
 class AddCountdownToSubscription < ActiveRecord::Migration
   def change
-  	add_column :users, :subscription_status_initial, :string
+  	#add_column :users, :subscription_status_initial, :string
   	add_column :subscription_applications, :approved_at, :datetime
   	add_column :subscription_application, :deadline, :datetime
   end
