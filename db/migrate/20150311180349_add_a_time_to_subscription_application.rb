214cfdf949ec1bf71428c143986c654ee0ee4607
fariliang <fariliang@gmail.com>
2015-03-20 22:35:45 -0400

    count down

--- db/migrate/20150311180349_add_a_time_to_subscription_application.rb
+++ db/migrate/20150311180349_add_a_time_to_subscription_application.rb
@@ -1,6 +1,6 @@
 class AddATimeToSubscriptionApplication < ActiveRecord::Migration
   def change
-  	add_column :subscription_applications, :approved_at, :datetime
+  	#add_column :subscription_applications, :approved_at, :datetime
   	add_column :subscription_applications, :completed_at, :datetime
   end
 end
