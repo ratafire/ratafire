f406863df86d5c3c7f4219e242305ef7263bd030
fariliang <fariliang@gmail.com>
2015-05-08 16:36:57 -0400

    Facebook update

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -300,11 +300,11 @@ Devise.setup do |config|
   require "omniauth-facebook"
   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development?
   if Rails.env.production?
-    config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],{:scope => 'email, user_about_me, user_location, user_website, user_education_history,read_stream, user_photos'}
+    config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],{:scope => 'email, user_about_me, user_location, user_website, user_education_history'}
     config.omniauth :facebookpages, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],{:scope => 'manage_pages,read_stream'}
     #On Engine Yard Cloud servers, the CA file is located at /etc/ssl/certs/ca-certificates.crt
   else
-    config.omniauth :facebook, ENV['FACEBOOK_KEY_TEST'], ENV['FACEBOOK_SECRET_TEST'],{:scope => 'email, user_about_me, user_location, user_website, user_education_history,read_stream, user_photos'}
+    config.omniauth :facebook, ENV['FACEBOOK_KEY_TEST'], ENV['FACEBOOK_SECRET_TEST'],{:scope => 'email, user_about_me, user_location, user_website, user_education_history'}
     config.omniauth :facebookpages, ENV['FACEBOOK_KEY_TEST'], ENV['FACEBOOK_SECRET_TEST'],{:scope => 'manage_pages, read_stream'}
   end
 
