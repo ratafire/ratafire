d5abe4f6f3c2103207fff4dffcae28689cd6b76f
fariliang <fariliang@gmail.com>
2017-01-16 05:07:36 -0500

    s3 direct upload

--- app/assets/javascripts/template_limitless.js
+++ app/assets/javascripts/template_limitless.js
@@ -4,10 +4,9 @@
 //= require jquery-ui.min
 //= require bootstrap.min
 //= require confirmation.js
-//= require s3_direct_upload
 //= require ahoy
 //= require jquery_nested_form
-//= require s3_direct_uoload
+//= require s3_direct_upload.js.coffee
 //= require_tree ./datatables
 //= require_tree ./selects
 //= require_tree ./template_limitless
