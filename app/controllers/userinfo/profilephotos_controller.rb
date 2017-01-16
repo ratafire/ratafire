c4930363a026e20dca8cebe5cd4eaf485524da75
fariliang <fariliang@gmail.com>
2017-01-16 05:36:14 -0500

    upload profile photo

--- app/controllers/userinfo/profilephotos_controller.rb
+++ app/controllers/userinfo/profilephotos_controller.rb
@@ -19,11 +19,11 @@ class Userinfo::ProfilephotosController < ApplicationController
 		@profilephoto.direct_upload_url = params[:profilephoto][:direct_upload_url]
 		@profilephoto.set_upload_attributes
 		@profilephoto.update(profilephoto_params)
-		#if @profilephoto.transfer_and_cleanup
-		#	@user_update_profilephoto = true
-		#else
-		#	flash[:error] = "Unable to update profile photo."
-		#end
+		if @profilephoto.transfer_and_cleanup
+			@user_update_profilephoto = true
+		else
+			flash[:error] = "Unable to update profile photo."
+		end
 	end
 
 	#userinfo_profilephoto DELETE
@@ -36,7 +36,7 @@ class Userinfo::ProfilephotosController < ApplicationController
 protected
 
 	def profilephoto_params
-		params.require(:profilephoto).permit(:image, :direct_upload_url)
+		params.require(:profilephoto).permit(:image, :direct_upload_url, :user_uid)
 	end	
 
 	def load_user
