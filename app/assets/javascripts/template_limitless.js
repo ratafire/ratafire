a338ba46ea12aab9232e98c4ccbbc65e51219973
fariliang <fariliang@gmail.com>
2017-01-30 18:58:30 -0500

    js

--- app/assets/javascripts/template_limitless.js
+++ app/assets/javascripts/template_limitless.js
@@ -13,6 +13,13 @@
 //= require jquery.magnific-popup.min
 //= require_tree ./s3_direct_upload
 //= require_tree ./velocity
+//= require_tree ./moment
+//= require_tree ./video
+//= require_tree ./visualization
+//= require_tree ./audio
+//= require_tree ./tables
+//= require_tree ./social
+//= require_tree ./pickers
 //= require_tree ./datatables
 //= require_tree ./validation
 //= require_tree ./editor
