5620d0afa754b1f11805728d4b3f86f703f35f37
fariliang <fariliang@gmail.com>
2017-01-30 20:53:44 -0500

    activity

--- vendor/gems/public_activity/lib/public_activity/models/activity.rb
+++ vendor/gems/public_activity/lib/public_activity/models/activity.rb
@@ -1,4 +1,9 @@
 module PublicActivity
   class Activity < inherit_orm("Activity")
+    acts_as_readable :on => :created_at  	
+  	acts_as_taggable
+  	acts_as_taggable_on :liker, :watcher
+  	default_scope { where(:deleted => false) }
+  	scope :active, -> { where(:deleted => false) }
   end
 end
